1780362cfd4a1f22b2361f1459d6b3dba985b745
bala-amypo <bala@amypo.in>
2025-12-17 08:11:58 +0000

    User commit

--- src/main/java/com/example/demo/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/newentity/NewfileEntity.java
@@ -1,18 +1,42 @@
 package com.example.demo.newentity;
 
-import jakarta.persistence. GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import jakarta.validation. constraints.NotBlank;
+import jakarta. validation. constraints. Email;
 
-public class NewfileEntity {
+public class NewfileEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "should not contain spaces")
+    @NotBlank(message="Should not contain spaces")
     private String name;
-    @NotBlank(message = "no blank allowed")
-    @Email(message="invalid format")
+    @NotBlank(message="no blank spaces")
+    @Email(message="invalid")
     private String email;
+    public NewfileEntity(Long id, @NotBlank(message = "Should not contain spaces") String name,
+            @NotBlank(message = "no blank spaces") @Email(message = "invalid") String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public NewfileEntity() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
